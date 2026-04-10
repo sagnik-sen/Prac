05ed00ff0d0346aa83c73245333667d92fd9dc20
Sagnik Sen <sen.sagnik9@gmail.com>
2026-04-11 03:50:05 +0530

    initial

---
+++ hamming.c
@@ -0,0 +1,31 @@
+// client_hamming.c
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main() {
+
+    int sock;
+    struct sockaddr_in addr;
+
+    char codeword[] = "10101100101";
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+
+    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+
+    send(sock, codeword, strlen(codeword), 0);
+
+    close(sock);
+
+    return 0;
+}
